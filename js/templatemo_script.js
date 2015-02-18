9804830f58e92c08d125bf9ff8b39483f4aec6e8
Álvaro Lupa <alvaro@pynapple.cl>
2015-02-18 14:13:56 -0300

    Commit inicial

---
+++ js/templatemo_script.js
@@ -0,0 +1,96 @@
+var top_menu_height = 0;
+jQuery(function($) {
+		$(window).load( function() {
+			$('.external-link').unbind('click');	
+		});
+		
+        $(document).ready( function() {
+
+            // load google map
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = '//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+            'callback=initialize';
+        document.body.appendChild(script);
+
+        top_menu_height = $('.templatemo-top-menu').height();
+        // scroll spy to auto active the nav item
+        $('body').scrollspy({ target: '#templatemo-nav-bar', offset: top_menu_height + 10 });
+		$('.external-link').unbind('click');
+
+        // scroll to top
+        $('#btn-back-to-top').click(function(e){
+            e.preventDefault();
+            scrollTo('#templatemo-top');
+        });
+
+        // scroll to specific id when click on menu
+        $('.templatemo-top-menu .navbar-nav a').click(function(e){
+            e.preventDefault(); 
+            var linkId = $(this).attr('href');
+            scrollTo(linkId);
+            if($('.navbar-toggle').is(":visible") == true){
+                $('.navbar-collapse').collapse('toggle');
+            }
+            $(this).blur();
+            return false;
+        });
+
+        // to stick navbar on top
+        $('.templatemo-top-menu ').stickUp();
+
+        // gallery category
+        $('.templatemo-gallery-category a').click(function(e){
+            e.preventDefault(); 
+            $(this).parent().children('a').removeClass('active');
+            $(this).addClass('active');
+            var linkClass = $(this).attr('href');
+            $('.gallery').each(function(){
+                if($(this).is(":visible") == true){
+                   $(this).hide();
+                };
+            });
+            $(linkClass).fadeIn();  
+        });
+
+        //gallery light box setup
+        $('a.colorbox').colorbox({
+                                    rel: function(){
+                                        return $(this).data('group');
+
+                                    }
+        });
+
+        $("#inline").colorbox({inline:true, width:"80%", fixed:true});
+        $("#inline2").colorbox({inline:true, width:"80%", fixed:true});
+        $("#info").colorbox({iframe:true, width:"80%", height:"80%", fixed:true});
+
+    });
+});
+
+function initialize() {
+    var location = new google.maps.LatLng(-20.2106571,-70.1491004);
+    var mapOptions = {
+      zoom: 17,
+      center: location
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    var marker = new google.maps.Marker({
+          position: location,
+          map: map
+    });
+
+    marker.setMap(map);
+}
+
+// scroll animation 
+function scrollTo(selectors)
+{
+
+    if(!$(selectors).size()) return;
+    var selector_top = $(selectors).offset().top - top_menu_height;
+    $('html,body').animate({ scrollTop: selector_top }, 'slow');
+
+}
